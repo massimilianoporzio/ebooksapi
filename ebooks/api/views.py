8b1c7bb35a9ee3b2be5dc18f0f8e831d9ff00ff4
Massimiliano <massimiliano.porzio@gmail.com>
2021-06-01 09:43:17 +0200

    permissions !

--- ebooks/api/views.py
+++ ebooks/api/views.py
@@ -1,36 +1,48 @@
 from rest_framework import generics
 from rest_framework import mixins
+from rest_framework import permissions
 from django.shortcuts import get_object_or_404
+from rest_framework.exceptions import ValidationError
+
 from ebooks.models import Ebook, Review
 from ebooks.api.serializers import ReviewSerializer, EbookSerializer
 from django.conf import settings
 from django.contrib.auth.models import User
+from ebooks.api.permissions import IsAdminUserOrReadOnly, IsReviewAuthorOrReadOnly
 
 
 class EBookListCreateAPIView(generics.ListCreateAPIView):
 
     queryset = Ebook.objects.all()
     serializer_class = EbookSerializer
+    permission_classes = [IsAdminUserOrReadOnly]
 
 
 class EbookDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Ebook.objects.all()
     serializer_class = EbookSerializer
+    permission_classes = [IsAdminUserOrReadOnly]
 
 
 class ReviewCreateAPIView(generics.CreateAPIView):
 
     queryset = Review.objects.all()
     serializer_class = ReviewSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
     def perform_create(self, serializer):
         ebook_pk = self.kwargs.get("ebook_pk")
         ebook = get_object_or_404(Ebook,pk=ebook_pk)
-        author = User.objects.first()
-        serializer.save(ebook=ebook,review_author=author)
+        review_author = self.request.user
+        review_queryset = Review.objects.filter(ebook=ebook, review_author=review_author)
+        if review_queryset.exists():
+            raise ValidationError("Hai già recensito questo ebook!!!")
+
+        serializer.save(ebook=ebook,review_author=review_author)
 
 
 class ReviewDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
 
     queryset = Review.objects.all()
     serializer_class = ReviewSerializer
+    permission_classes = [IsReviewAuthorOrReadOnly]
